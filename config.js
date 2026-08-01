3c54f35a0d3a8a3cae465524f8dc51a50fac404f
blackburndesigned <blackburndesigned@gmail.com>
2026-08-01 17:44:59 -0400

    Update supabaseAnonKey in config.js
    Updated the supabaseAnonKey in the configuration.

--- config.js
+++ config.js
@@ -4,7 +4,6 @@
  */
 window.CC_CONFIG = {
   supabaseUrl: "https://njesvsdhndtthbsctmkp.supabase.co",
-  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qZXN2c2RobmR0dGhic2N0bWtwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU1MzI4NDksImV4cCI6MjEwMTEwODg0OX0.mkwZ6tsDlVi_t6umhjvj3remdDzMACCi2wRUVTtHU1o
-",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qZXN2c2RobmR0dGhic2N0bWtwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU1MzI4NDksImV4cCI6MjEwMTEwODg0OX0.mkwZ6tsDlVi_t6umhjvj3remdDzMACCi2wRUVTtHU1o",
   showShopTools: false,
 };
